dc0f25473a38943cd3d5eee66a9db87ecd751215
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2020-01-15 13:54:46 +0800

    Update Mongo.py

--- Mongo.py
+++ Mongo.py
@@ -5,23 +5,9 @@ ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 bot = Bot (ACCESS_TOKEN)
 
 
-def get_data_users(users, sender_id):
-    a = users.find_one({'user_id': sender_id})
-    if a != None:
-        return a
-    return None
-def get_data_patient(patient, sender_id):
-    a = patient.find_one({'user_id': sender_id})
-    if a != None:
-        return a
-    return None
 
-def set_terms(users, sender_id,yes):
-    users.update({"user_id": sender_id},{"$set":{"accept_disclaimer": yes}})
-def set_ask(users, sender_id, ask):
-    users.update({"user_id": sender_id},{"$set":{"last_message_ask": ask}})
-def set_answer(users, sender_id, answer):
-    users.update({"user_id": sender_id},{"$set":{"last_message_answer": answer}})
+def set_message(users, sender_id,column, value):
+    users.update({"user_id": sender_id},{"$set":{column : value}})
     
 def find_user_id(users, user_object_id):
     # Convert from string to ObjectId:
@@ -40,47 +26,117 @@ def create_user(users, sender_id, user_fb):
     timestamp = datetime.strftime(datetime.now(),"%Y-%m-%d %H:%M:%S")
     user_insert = {'user_id': sender_id, 
                     'created_at': timestamp,
-                    'last_seen': "1970-01-01 00:00:00",
                     'first_name':user_fb['first_name'],
                     'last_name':user_fb['last_name'],
+                    'name':'',
+                    'std_id':'',
                     'last_message_ask':'None',
-                    'last_message_answer':'None',
-                    'accept_disclaimer':'No'
+                    'last_message_answer':'None'
                    }
     users.insert(user_insert)
-
-
-# Manual input
-def create_patient(patient, sender_id, name, age, weight, relation, count, total,symptoms):
-    spatient = patient.find_one({'user_id': sender_id})
-    if spatient is None:                      
-        timestamp = datetime.strftime(datetime.now(),"%Y-%m-%d %H:%M:%S")
-        patient_insert = {'user_id': sender_id, 
-                        'created_at': timestamp,
-                        'name': name,
-                        'age':age,
-                        'weight': weight,
-                        'relation': relation,
-                        'count_yes': count,
-                        'total_symptoms': total,
-                        'symptoms': symptoms
-                        }
-        patient.insert(patient_insert)
-    else:
-        patient.update({"user_id": sender_id},{"$set":{'name': name, 'age':age, 'weight':weight, 'relation':relation, 'count_yes':count, 'total_symptoms':total, 'symptoms': symptoms}})                      
     
-def set_patient(patient, sender_id, column, value):
-    patient.update({"user_id": sender_id},{"$set":{column: value}})
+def get_data(table, std_id):
+    a = table.find({'std_id': std_id})
+    if a != None:
+        return a
+    return None    
 
-# Input: Facebook's sender_id
-def get_user_mongo(users, sender_id):
-    return users.find_one({'user_id': sender_id})
+def update_user(student,std_id, name, ask, answer):
+    student.update({"std_id": std_id},{"$set":{'name':name,'std_id':std,'last_message_ask':ask,'last_message_answer':answer}})                      
+    
+def create_student(student,std_id, name, guardian, contact, address, email):                     
+    timestamp = datetime.strftime(datetime.now(),"%Y-%m-%d %H:%M:%S")
+    insert = { 'created_at': timestamp,
+                        'name': name,
+                        'std_id':std_id,
+                        'contact': contact,
+                        'address': address,
+                        'email': email,
+                        'guardian': guardian
+                        }
+    student.insert(insert)
+def update_student(student,std_id, name, guardian, contact, address, email):
+    student.update({"std_id": std_id},{"$set":{'name': name,'std_id':std_id, 'contact': contact,'address': address,'email': email,'guardian': guardian}})                      
+    
 
-def update_last_seen(users, sender_id):
-    now = datetime.now()
-    timestamp = datetime.strftime(now,"%Y-%m-%d %H:%M:%S")
-    users.update({"user_id": sender_id},{"$set":{"last_seen": timestamp}})
+    
+def create_schedule(schedule, std_id, subject, day, time, room, unit, instructor,sy,sem):                     
+    insert = { 'std_id': std_id,
+                        'subject':std_id,
+                        'day': day,
+                        'time': time,
+                        'room': room,
+                        'unit': unit,
+                        'instructor': instructor,
+                        'sy': sy,
+                        'sem': sem
+                        }
+    schedule.insert(insert)
 
-
-
-
+def create_account(account, std_id, sy, sem, prelim, midterm, prefinal, final, amount_paid,balance, old_account):                     
+    insert = { 'std_id': std_id,
+                        'subject':std_id,
+                        'prelim': prelim,
+                        'midterm': midterm,
+                        'prefinal': prefinal,
+                        'final': final,
+                        'amount_paid': amount_paid,
+                        'balance': balance,
+                        'old_account': sy,
+                        'sy': sy,
+                        'sem': sem
+                        }
+    account.insert(insert)
+    
+def create_exam(exam, std_id, subject, day, time, room, unit, proctor,sy,sem):                     
+    insert = { 'std_id': std_id,
+                        'subject':std_id,
+                        'day': day,
+                        'time': time,
+                        'room': room,
+                        'unit': unit,
+                        'proctor': proctor,
+                        'sy': sy,
+                        'sem': sem
+                        }
+    exam.insert(insert)
+    
+def create_scholarship(scholarship,name,description,status):                     
+    insert = {      
+                        'name': name,
+                        'description': description,
+                        'status': status
+                        }
+    scholarship.insert(insert)
+        
+def create_program(programs,name,description,department,status):                     
+    insert = { 
+                        'name': name,
+                        'description': description,
+                        'department':department,
+                        'status': status
+                        }
+    programs.insert(insert)
+    
+def get_details(table):
+    a = table.find()
+    if a != None:
+        return a
+    return None      
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
